c1c0bf209b0303ec8365a7bd23017450658b0b02
Konstantinos Larkou <klarko01@ucy.ac.cy>
2023-03-01 18:06:00 +0100

    results from running on chimera

--- validate_DBR.py
+++ validate_DBR.py
@@ -1,7 +1,8 @@
 
 from DBR import DBR
 import networkx as nx
-from MVC_CQM import minimum_vertex_cover_cqm
+from MVC_chimera import minimum_vertex_cover_chimera
+from MVC_pegasus import minimum_vertex_cover_pegasus
 import copy
 import time
 
@@ -18,37 +19,45 @@ def minimum_vertex_cover_exact_solve_np_hard(G):
 	MC = maximum_clique_exact_solve_np_hard(GC)
 	return list(set(nodes)-set(MC))
 
-f = open("chimera.txt", "w")
+f = open("density.txt", "w")
+for i in range(8): #10 - 125
+    
+    G = nx.gnp_random_graph(50, 0.1 + i * 0.1) 
+    G2 = copy.deepcopy(G)
+    G3 = copy.deepcopy(G)
 
-for j in range(10):
-    for i in range(23): #10 - 125
-        
-        G = nx.gnp_random_graph(i * 5 + 10, 0.2) 
-        G2 = copy.deepcopy(G)
+    print("Original solution solution")
+    print("Graph size: ", len(G), "\n")
+    #soln = len(minimum_vertex_cover_exact_solve_np_hard(G2))
+    start = time.time()
+    solution_original = DBR(G, 10, minimum_vertex_cover_exact_solve_np_hard)
+    end = time.time()
+    time_original = end - start
+    #assert len(solution) == soln
+    
+    print("Pegasus solution")
+    print("Graph size: ", len(G2), "\n")
+    start = time.time()
+    solution_pegasus = DBR(G2, 10, minimum_vertex_cover_pegasus)
+    end = time.time()
+    time_pegasus = end - start
 
-        print("Original solution solution")
-        print("Graph size: ", len(G), "\n")
-        #soln = len(minimum_vertex_cover_exact_solve_np_hard(G2))
-        start = time.time()
-        solution_original = DBR(G, j * 5 + 10, minimum_vertex_cover_exact_solve_np_hard)
-        end = time.time()
-        time_original = end - start
-        #assert len(solution) == soln
-        
-        print("CQM solution")
-        print("Graph size: ", len(G2), "\n")
-        start = time.time()
-        solution_cqm = DBR(G2, j * 5 + 10, minimum_vertex_cover_cqm)
-        end = time.time()
-        time_cqm = end - start
+    print("Chimera solution")
+    print("Graph size: ", len(G3), "\n")
+    start = time.time()
+    solution_chimera = DBR(G3, 10, minimum_vertex_cover_chimera)
+    end = time.time()
+    time_chimera = end - start
 
-        print("Original solution: ", solution_original)
-        print("Length of Original solution: ", len(solution_original), "\n")
-        print("CQM solution: ", solution_cqm)
-        print("Length of cqm solution: ", len(solution_cqm),"\n")
-        #assert len(solution) == nx.graph_clique_number(G)
-        f.write("len Ori: " + str(len(solution_original)) + " len CQM: " + str(len(solution_cqm)))
-        f.write(" Time Ori: " + str(time_original) + " Time CQM: " + str(time_cqm))
-        f.write(" Limit: " + str(j * 5 + 10) + "\n")        
-        #assert len(solution_cqm) == len(solution_original)
+    print("Original solution: ", solution_original)
+    print("Length of Original solution: ", len(solution_original), "\n")
+    print("Pegasus solution: ", solution_pegasus)
+    print("Length of pegasus solution: ", len(solution_pegasus),"\n")
+    print("Chimera solution: ", solution_chimera)
+    print("Length of chimera solution: ", len(solution_chimera),"\n")
+    #assert len(solution) == nx.graph_clique_number(G)
+    f.write("len Ori: " + str(len(solution_original)) + " len pegasus: " + str(len(solution_pegasus)) + " len chimera: " + str(len(solution_chimera)))
+    f.write(" Time Ori: " + str(time_original) + " Time pegasus: " + str(time_pegasus) + " Time chimera: " + str(time_chimera))
+    f.write(" Limit: " + str(10) + "\n")        
+    #assert len(solution_pegasus) == len(solution_original)
 f.close()
